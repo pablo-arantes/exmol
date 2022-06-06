c4592bd65602c67bfd1fa78f1fa54d43916981af
Pablo Ricardo Arantes <35934150+pablo-arantes@users.noreply.github.com>
2022-06-06 11:25:07 -0700

    Update plot_utils.py

--- exmol/plot_utils.py
+++ exmol/plot_utils.py
@@ -46,7 +46,8 @@ def insert_svg(
     size = mol_size
     mol_svgs = _mol_images(exps, mol_size, mol_fontsize, True)
     rewrites = {f"rdkit-img-{i}": v for i, v in enumerate(mol_svgs)}
-    svg = skunk.pltsvg(bbox_inches="tight")
+#     svg = skunk.pltsvg(bbox_inches="tight")
+    svg = skunk.pltsvg(pad_inches="tight")
     return skunk.insert(rewrites, svg=svg)
 
 
